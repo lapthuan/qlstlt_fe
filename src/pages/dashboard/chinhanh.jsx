9c8db73ce8699c8131b2d400e7d1298e50a836c3
thuan <lapthuan0805@gmail.com>
2023-11-25 09:52:02 +0700

    aaaaaaa

--- src/pages/dashboard/chinhanh.jsx
+++ src/pages/dashboard/chinhanh.jsx
@@ -12,10 +12,6 @@ import {
 
 import { authorsTableData } from "@/data";
 import useAsync from "@/hook/useAsync";
-import ServiceHangHoa from "@/service/ServiceHangHoa";
-import { Link } from "react-router-dom";
-import { Button, message, Popconfirm } from "antd";
-import ServiceSieuThi from "@/service/ServiceSieuThi";
 import ServiceBranch from "@/service/ServiceBranch";
 
 
@@ -38,14 +34,14 @@ const ChiNhanh = () => {
                 <CardHeader variant="gradient" color={"gray"
                 } className="mb-8 p-6">
                     <Typography variant="h6" color={"white"}>
-                        Danh sách siêu thị
+                        Danh sách chi nhánh
                     </Typography>
                 </CardHeader>
                 <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
                     <table className="w-full min-w-[640px] table-auto">
                         <thead>
                             <tr>
-                                {["Mã siêu thị", "Tên siêu thị", "Địa chỉ", "Số điện thoại", "Email", "Mã chi nhánh"].map((el) => (
+                                {["Mã chi nhánh", "Tên chi nhánh", "Địa chỉ", "Tên tỉnh"].map((el) => (
                                     <th
                                         key={el}
                                         className="border-b border-blue-gray-50 py-3 px-5 text-left"
